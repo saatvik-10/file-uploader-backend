4cbf95ced0adcfb826f886253db866b1b1345745
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-08-27 16:08:04 +0530

    updated environments

--- src/types/environments.d.ts
+++ src/types/environments.d.ts
@@ -3,8 +3,6 @@ declare global {
     interface ProcessEnv {
       PORT: string
       MONGODB_URI: string
-      JWT_SECRET: string
-      ADMIN_JWT_SECRET: string
       S3_ACCESS_KEY: string
       S3_SECRET_KEY: string
       S3_REGION: string
