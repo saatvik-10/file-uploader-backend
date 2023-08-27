b6028f77f452f3cc3bdf7141bdcbe66a216dfbd1
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-08-27 15:51:02 +0530

    removed unecessary files

--- src/interfaces/example.interface.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from 'express'
-
-export interface IExampleController {
-  getExample(req: Request, res: Response, next: NextFunction): Promise<void>
-}
-
-export interface IExampleService {
-  getExamples(): Promise<IExample[]>
-}
